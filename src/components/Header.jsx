fe1d5fb61419b9c21d5ea7e842d9eec5dd18cfe5
Alberto Zavala <jzavala@efim.com>
2021-10-20 10:48:38 -0700

    Se agregó checkout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import Menu from '@components/Menu'
-
+import AppContext from '../context/AppContext'
+import MyOrder from '../containers/MyOrder'
 import '@styles/Header.scss'
 import menu from '@icons/icon_menu.svg'
 import logo from '@logos/logo_yard_sale.svg'
@@ -8,7 +9,9 @@ import shoppingCart from '@icons/icon_shopping_cart.svg'
 
 
 const Header = () => {
-  const [toggle, setToggle] = useState(false);
+  const [ toggle, setToggle ] = useState(false);
+  const [ toggleOrders, setToggleOrders ] = useState(false);
+  const { state } = useContext(AppContext);
 
   const handleToggle = () => {
     setToggle(!toggle);
@@ -45,16 +48,23 @@ const Header = () => {
 
     <div className="navbar-right">
       <ul>
-        <li className="navbar-email" onClick={ handleToggle }>platzi@example.com</li>
-        <li className="navbar-shopping-cart">
-          <img src={shoppingCart} alt="shopping cart"/>
-          <div>2</div>
+        <li 
+          className="navbar-email" 
+          onClick={ handleToggle }>
+            platzi@example.com
+        </li>
+        <li 
+          className="navbar-shopping-cart" 
+          onClick={() => setToggleOrders(!toggleOrders) }>
+            <img src={shoppingCart} alt="shopping cart"/>          
+            {
+              state.cart.length > 0 && <div>${state.cart.length}</div>
+            }          
         </li>
       </ul>
     </div>
-    {
-      toggle && <Menu/>
-    }    
+    { toggle && <Menu/> }
+    { toggleOrders && <MyOrder/> }    
 
   </nav>
   )
