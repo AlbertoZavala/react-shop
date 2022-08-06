7436b436e77d109e0381d75290ed75e6d584bc69
Jose Alberto Zavala Flores <jose.alberto.zavala.flores@gmail.com>
2022-08-06 14:58:44 -0700

    Change home path

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
 			<BrowserRouter>
 				<Layout>
 					<Switch>
-						<Route exact path="/" component={Home} />
+						<Route exact path="/home" component={Home} />
 						<Route exact path="/login" component={Login} />
 						<Route exact path="/password-recovery" component={PasswordRecovery} />
 						<Route exact path="/send-email" component={SendEmail} />
